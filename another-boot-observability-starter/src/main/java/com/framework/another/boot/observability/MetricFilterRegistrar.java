7d4bb58ba5165ea91c453b354b51bd69ce9d3342
Rohan Aditeya <rohan.aditeya@gmail.com>
2024-04-21 17:56:12 +0530

    removed spring cloud kube as it does not have native image support yet and used k8 fabric client.

--- another-boot-observability-starter/src/main/java/com/framework/another/boot/observability/MetricFilterRegistrar.java
+++ another-boot-observability-starter/src/main/java/com/framework/another/boot/observability/MetricFilterRegistrar.java
@@ -2,12 +2,13 @@ package com.framework.another.boot.observability;
 
 import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.config.MeterFilter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
 import org.springframework.boot.cloud.CloudPlatform;
-import org.springframework.cloud.kubernetes.commons.PodUtils;
 import org.springframework.context.annotation.Bean;
 
 import java.util.HashSet;
@@ -17,13 +18,17 @@ import java.util.Set;
 @ConditionalOnCloudPlatform(value = CloudPlatform.KUBERNETES)
 public class MetricFilterRegistrar {
 
+    private static final String HOSTNAME_ENV_NAME = "HOSTNAME";
+
     @Bean
-    public MeterFilter kubernetesLabelRegistrars (PodUtils<Pod> podPodUtils) {
-        Pod currentPod = podPodUtils.currentPod().get();
-        ObjectMeta podMeta = currentPod.getMetadata();
-        Set<Tag> commonTagSet = new HashSet<>();
-        commonTagSet.add(Tag.of("namespace", podMeta.getNamespace()));
-        commonTagSet.add(Tag.of("podName", podMeta.getName()));
-        return MeterFilter.commonTags(commonTagSet);
+    public MeterFilter kubernetesLabelRegistrars () {
+        try (KubernetesClient kubernetesClient = new KubernetesClientBuilder().build()) {
+            Pod currentPod = kubernetesClient.pods().withName(System.getenv(HOSTNAME_ENV_NAME)).get();
+            ObjectMeta podMeta = currentPod.getMetadata();
+            Set<Tag> commonTagSet = new HashSet<>();
+            commonTagSet.add(Tag.of("namespace", podMeta.getNamespace()));
+            commonTagSet.add(Tag.of("podName", podMeta.getName()));
+            return MeterFilter.commonTags(commonTagSet);
+        }
     }
 }
