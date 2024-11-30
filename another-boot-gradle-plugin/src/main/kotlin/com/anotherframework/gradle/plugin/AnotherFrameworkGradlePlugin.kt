62d620362660ec87603d7e1e15d78020ec396974
rohan-aditeya <rohan.aditeya@gmail.com>
2024-11-30 17:25:32 +0530

    backed off spring boot plugin for library.

--- another-boot-gradle-plugin/src/main/kotlin/com/anotherframework/gradle/plugin/AnotherFrameworkGradlePlugin.kt
+++ another-boot-gradle-plugin/src/main/kotlin/com/anotherframework/gradle/plugin/AnotherFrameworkGradlePlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.gradle.api.plugins.JavaLibraryPlugin
 import org.springframework.boot.gradle.util.VersionExtractor
 
 class AnotherFrameworkGradlePlugin: Plugin<Project> {
@@ -13,13 +14,27 @@ class AnotherFrameworkGradlePlugin: Plugin<Project> {
     private val anotherBootBOMCoordinates: String = "com.framework.another.boot:another-boot-bom:${anotherBootVersion}"
 
     override fun apply(target: Project) {
-        target.pluginManager.apply(SpringBootPlugin::class.java)
-        target.pluginManager.apply(DependencyManagementPlugin::class.java)
-        target.extensions.apply {
-            val dependencyManagementExtension = getByType(DependencyManagementExtension::class.java)
-            dependencyManagementExtension.apply {
-                imports { importsHandler ->
-                    importsHandler.mavenBom(anotherBootBOMCoordinates)
+        val isLibraryPluginApplied = target.plugins.findPlugin(JavaLibraryPlugin::class.java) != null
+        if (isLibraryPluginApplied) {
+            target.pluginManager.apply(DependencyManagementPlugin::class.java)
+            target.extensions.apply {
+                val dependencyManagementExtension = getByType(DependencyManagementExtension::class.java)
+                dependencyManagementExtension.apply {
+                    imports { importsHandler ->
+                        importsHandler.mavenBom(anotherBootBOMCoordinates)
+                        importsHandler.mavenBom(SpringBootPlugin.BOM_COORDINATES)
+                    }
+                }
+            }
+        } else {
+            target.pluginManager.apply(SpringBootPlugin::class.java)
+            target.pluginManager.apply(DependencyManagementPlugin::class.java)
+            target.extensions.apply {
+                val dependencyManagementExtension = getByType(DependencyManagementExtension::class.java)
+                dependencyManagementExtension.apply {
+                    imports { importsHandler ->
+                        importsHandler.mavenBom(anotherBootBOMCoordinates)
+                    }
                 }
             }
         }
