c222ae6373adcc89b894d82c1c8294a0aebc1dcb
rohan-aditeya <rohan.aditeya@gmail.com>
2024-12-15 21:35:39 +0530

    removed spring web in observability.

--- another-boot-observability-starter/build.gradle.kts
+++ another-boot-observability-starter/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
     api(project(":another-boot-starter"))
     api("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web") //keep optional so that web is not enforced.
     api("io.micrometer:micrometer-tracing-bridge-otel")
     api("io.opentelemetry:opentelemetry-exporter-otlp")
     api("io.micrometer:micrometer-registry-otlp")
